b222d47116d4ba5e9dac216e3f67b95c56d3abf0
Quewata <87590631+Quewata@users.noreply.github.com>
2021-07-18 16:08:13 -0700

    Add files via upload

---
+++ snek.cpp
@@ -0,0 +1,118 @@
+//screen setup // ########
+int(a + a == bg.png) {
+    jmp(b == b){
+        cout(bg.png){
+
+        }
+    }
+}
+cin(a + a == b | b + a == a) {
+    cout(if b == b) {
+        jmp(a, b == cout) {
+            int(a == b, +1 == cout) {
+
+            }
+        }
+    }
+}
+//score // ########
+from cin(a + a == b | b + a == a) {
+    cout(if b == b) {
+        jmp(a, b == cout) {
+            int(a == b, +1 == cout) {
+
+            }
+
+        }
+    }
+}
+then() {
+    int(a + b == 0,+1) {
+
+    }
+}
+//ui // ########
+if(cin == Up) {
+    jmp() {
+        if(cin == Down) {
+            jmp() {
+                if(cin == Left) {
+                    jmp() {
+                        if(cin == Right) {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+cout(){
+
+}
+//snek // ########
+int(a + b == cout) {
+    jmp(b + a == cout) {
+        int(a - b == cout)
+        _loop() {
+
+        }
+    }
+}
+//function // ########                                                              // // ########
+int(a + b == cout) {
+    jmp(b + a == cout) {
+        int(a - b == cout)
+        _loop() {
+            jmp(s + f == g) {
+                int(s + f == g) {
+
+                }
+            }            
+        }
+    }
+}
+int(-a + b == g) {
+
+}
+//score // ########
+int(a + b == cout) {
+    jmp(b + a == cout) {
+        int(a - b == cout)
+        _loop() {
+            jmp(s + f == g) {
+                int(s + f == g) {
+                    _loop() {
+
+                    }
+                }
+            }            
+        }
+    }
+}
+cout(a, b = 0,+1) {
+
+}
+//border // ########
+cout(s + b == d) {
+    jmp() {
+        void(s == b == d) {
+
+        }
+    }
+}
+//win// lose // ########
+cout(s + b == d) {
+    jmp() {
+        void(s == b == d) {
+
+        }
+    }
+}
+_loop() {
+    more() {
+        done() {
+
+        }
+    }
+}
